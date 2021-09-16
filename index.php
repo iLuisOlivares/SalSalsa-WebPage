09f1143b582a134ee22cdfc497aa4d656680b973
Ro <robertt.yarce@gmail.com>
2021-09-16 08:34:28 -0500

    enlaces para las otras paginas

--- index.php
+++ index.php
@@ -1,51 +1,5 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
-<!--<head>
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mi primera página :3</title>
-
-    <!extenciones->
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
-        integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/"
-        crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-</head>-->
-
-<!--<body>
-    <header>
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light" style="height: 80px">
-            <a class="navbar-brand" href="#"></a>
-            <img class="logoo" id="logoPrincipal" src="">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul class="navbar-nav mr-auto" style="font-size: 20px">
-                    <li class="nav-item active">
-                        <a href="" class="nav-link">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-item nav-link ">Proyecto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-item nav-link">Nosotros</a>
-                    </li>
-                </ul>
-        </nav>
-        <!-Banner
-        <div class="banner" id="home" style="background: url('img/banner1.jpeg'); background-size: cover; "></div>-->
-
-
 <section class="seccionGeneralCarusel">
     <div class="contenedorGeneralCarrusel">
         <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
     <footer>
         <div class="contenedorGeneral">
             <footer class="footerCentrado">
-                <p class="textWhite parrafo">Hecho con amor por Robert Yarce Rincon</p>
+                <p class="textWhite parrafo">Hecho por el Grupo #4</p>
 
                 <div class="custom-element custom-element-networks">
                     <span class="element-tag">
@@ -10,22 +10,30 @@
                             <span class="textWhite parrafo">Sigueme en:</span>
                             <br>
                             <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.facebook.com/Rob.yarce/" target="_blank"><i
+                                    href="https://www.facebook.com/" target="_blank"><i
                                         class="fab fa-facebook"></i></a></li>
                             <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.facebook.com/Rob.yarce/" target="_blank"><i
+                                    href="https://www.twitter.com/" target="_blank"><i
                                         class="fab fa-twitter"></i></a></li>
                             <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.instagram.com/robertyarce/" target="_blank"><i
+                                    href="https://www.instagram.com" target="_blank"><i
                                         class="fab fa-instagram"></i></a></li>
                         </ul>
                     </span>
                 </div>
                 <div>
-                    <p class="parrafo textWhite">Copyright © Proyecto. Todos los derechos reservados.</p>
+                    <p class="parrafo textWhite">Copyright 2021 © Proyecto. Todos los derechos reservados.</p>
                 </div>
         </div>
         </footer>
     </section>
+    <script src="./js/contact.js"></script>
+     <!-- Option 1: Bootstrap Bundle with Popper -->
+     <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+      crossorigin="anonymous"
+    ></script>
+    <script type="text/javascript" src="./js/carta.js"></script>
 </body>
 </html>
