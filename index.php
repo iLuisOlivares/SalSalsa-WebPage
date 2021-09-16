e5aee6f7cd67639d2be702f72441c07cba806c04
Ro <robertt.yarce@gmail.com>
2021-09-16 08:02:57 -0500

    Avance

--- index.php
+++ index.php
@@ -46,169 +46,142 @@
         <div class="banner" id="home" style="background: url('img/banner1.jpeg'); background-size: cover; "></div>-->
 
 
-    <section class="seccionGeneralCarusel">
-        <div class="contenedorGeneralCarrusel">
-            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-                        aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active anchoImagenCa" data-bs-interval="10000">
-                        <img class="d-block w-100" src="img/fotoCar1.png" alt="First slide" class="anchoImagenCa"
-                            id="anchoImagenCaId">
-                        <div class="carousel-caption d-none d-md-block textCarrucel">
-                            <h5 class="textWhite anuncioCarucel">La pasión por lo que haces te hará entender que los
-                                límites son mentales.</h5>
-                            <p class="textWhite anuncioCarucel ">Anónimo </p>
-                        </div>
-                    </div>
-                    <div class="carousel-item" data-bs-interval="15000">
-                        <img class="d-block w-100" src="img/fotoCa.png" alt="First slide" class="anchoImagenCa"
-                            id="anchoImagenCaId">
-                        <div class="carousel-caption d-none d-md-block textCarrucel">
-                            <h5 class="textWhite anuncioCarucel">La mejor forma de predecir tu futuro, es construirlo.
-                            </h5>
-                            <p class="textWhite anuncioCarucel ">Alan Kay</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="img/fotoDescripcionDos.png" alt="First slide" class="anchoImagenCa"
-                            id="anchoImagenCaId">
-                        <div class="carousel-caption d-none d-md-block textCarrucel">
-                            <h5 class="textWhite anuncioCarucel ">No por miedo a errar, vas a dejar de jugar.</h5>
-                            <p class="textWhite anuncioCarucel ">Anónimo </p>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+<section class="seccionGeneralCarusel">
+    <div class="contenedorGeneralCarrusel">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-        </div>
-    </section>
-
-    <section class="seccionGeneralCarusel" id="">
-
-        <div class="contendor">
-            <img class="d-block w-100" src="img/fotodescripcionDos.png" alt="First slide" class="" id="">
-
-            <p class="textoEncima  textWhite ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
-                ducimus consectetur perspiciatis veniam odit facilis laborum aperiam. Laborum, possimus. Quasi culpa
-                laborum deleniti doloremque qui
-                autem maxime labore asperiores ut. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
-                ducimus consectetur perspiciatis veniam odit facilis laborum aperiam. Laborum, possimus. Quasi culpa
-                laborum deleniti doloremque qui
-                autem maxime labore asperiores ut Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quia obcaecati repellat animi nesciunt voluptates atque pariatur a, porro odio eaque rem eveniet asperiores laudantium eligendi. Provident corporis dolor dignissimos?</p>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <p class="textoEncima centrado textWhite">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, beatae?
-                Cumque laudantium reprehenderit iure quidem itaque rem a pariatur eveniet animi recusandae dignissimos
-                iusto ullam, deserunt maxime enim mollitia atque. Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Hic cumque quaerat tempore est saepe modi voluptatum quidem illo neque, fugit, explicabo dolorem, illum vel maxime laboriosam ratione voluptas distinctio tempora.</p>
-            <br>
-            <div class="centrado"></div>
-        </div>
-    </section>
-
-    <section class="seccionGeneralDos">
-        <div class="contenedorGeneral">
-            <h2 class="textCenter">Un poco de lo que hay</h2>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis obcaecati id explicabo tempora debitis doloremque voluptatum veritatis recusandae ratione in necessitatibus consequuntur eum excepturi nesciunt, facilis commodi. Ut, magnam earum?</p>
-                <div class="columna2Dos">
-                    <label class="labelRegular">Nuevas comidad:</label>
-                        <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
-
-                        <label class="labelRegular">Nuevos sabores:</label>
-                        <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+            <div class="carousel-inner">
+                <div class="carousel-item active anchoImagenCa" data-bs-interval="10000">
+                    <img class="d-block w-100" src="img/fotoCar1.png" alt="First slide" class="anchoImagenCa" id="anchoImagenCaId">
+                    <div class="carousel-caption d-none d-md-block textCarrucel">
+                        <h5 class="textWhite anuncioCarucel">La pasión por lo que haces te hará entender que los
+                            límites son mentales.</h5>
+                        <p class="textWhite anuncioCarucel ">Anónimo </p>
+                    </div>
                 </div>
-                <div class="columna2Dos">
-                    <label class="labelRegular">Nuevos gustos:</label>
-                    <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+                <div class="carousel-item" data-bs-interval="15000">
+                    <img class="d-block w-100" src="img/fotoCa.png" alt="First slide" class="anchoImagenCa" id="anchoImagenCaId">
+                    <div class="carousel-caption d-none d-md-block textCarrucel">
+                        <h5 class="textWhite anuncioCarucel">La mejor forma de predecir tu futuro, es construirlo.
+                        </h5>
+                        <p class="textWhite anuncioCarucel ">Alan Kay</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/fotoDescripcionDos.png" alt="First slide" class="anchoImagenCa" id="anchoImagenCaId">
+                    <div class="carousel-caption d-none d-md-block textCarrucel">
+                        <h5 class="textWhite anuncioCarucel ">No por miedo a errar, vas a dejar de jugar.</h5>
+                        <p class="textWhite anuncioCarucel ">Anónimo </p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</section>
 
-                    <label class="labelRegular">Nuevos espacios</label>
-                    <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+<section class="seccionGeneralCarusel" id="">
+
+    <div class="contendor">
+        <img class="d-block w-100" src="img/fotodescripcionDos.png" alt="First slide" class="" id="">
+
+        <p class="textoEncima  textWhite ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+            ducimus consectetur perspiciatis veniam odit facilis laborum aperiam. Laborum, possimus. Quasi culpa
+            laborum deleniti doloremque qui
+            autem maxime labore asperiores ut. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+            ducimus consectetur perspiciatis veniam odit facilis laborum aperiam. Laborum, possimus. Quasi culpa
+            laborum deleniti doloremque qui
+            autem maxime labore asperiores ut Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quia obcaecati repellat animi nesciunt voluptates atque pariatur a, porro odio eaque rem eveniet asperiores laudantium eligendi. Provident corporis dolor dignissimos?</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p class="textoEncima centrado textWhite">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, beatae?
+            Cumque laudantium reprehenderit iure quidem itaque rem a pariatur eveniet animi recusandae dignissimos
+            iusto ullam, deserunt maxime enim mollitia atque. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Hic cumque quaerat tempore est saepe modi voluptatum quidem illo neque, fugit, explicabo dolorem, illum vel maxime laboriosam ratione voluptas distinctio tempora.</p>
+        <br>
+        <div class="centrado"></div>
+    </div>
+</section>
+
+<section class="seccionGeneralDos">
+    <div class="contenedorGeneral">
+        <h2 class="textCenter">Un poco de lo que hay</h2>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis obcaecati id explicabo tempora debitis doloremque voluptatum veritatis recusandae ratione in necessitatibus consequuntur eum excepturi nesciunt, facilis commodi. Ut, magnam earum?</p>
+            <div class="columna2Dos">
+                <label class="labelRegular">Nuevas comidad:</label>
+                <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+
+                <label class="labelRegular">Nuevos sabores:</label>
+                <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+            </div>
+            <div class="columna2Dos">
+                <label class="labelRegular">Nuevos gustos:</label>
+                <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
+
+                <label class="labelRegular">Nuevos espacios</label>
+                <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 200px; height: 200px;">
             </div>
 
             <div class="columna2Dos">
                 <label class="labelRegular">Miralo</label>
                 <img class="" src="img/fotodescripcionDos.png" alt="First slide" class="" id="" style="width: 500px; height: 430px;">
-        </div>
-            
             </div>
-        </div>
-    </section>
 
-    <section class="seccionGeneral">
-        <div class="contenedorGeneral">
-            <h2 class="textCenter">Testimonios</h2>
-            <div class="testimonios" >
-                <div class="testimoniosDos">
-                <div class="columnaTestimonio borde">
-                    <img class="imagenesTestimonios" src="img/cuadrado.png" alt="First slide"  id="">
-                    <p class="textCenter">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nulla quibusdam officia rerum at, debitis reprehenderit et aut culpa accusamus recusandae. Cupiditate reprehenderit fuga praesentium voluptatibus voluptate corporis? Esse, quod.</p>
-                </div>
-                <div class="columnaTestimonio borde">
-                    <img class="imagenesTestimonios" src="img/cuadrado.png" alt="First slide"  id="">
-                    <p class="textCenter">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nulla quibusdam officia rerum at, debitis reprehenderit et aut culpa accusamus recusandae. Cupiditate reprehenderit fuga praesentium voluptatibus voluptate corporis? Esse, quod.</p>
-                </div>
-                <div class="columnaTestimonio borde">
-                    <img class="imagenesTestimonios" src="img/cuadrado.png" alt="First slide"  id="">
-                    <p class="textCenter">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nulla quibusdam officia rerum at, debitis reprehenderit et aut culpa accusamus recusandae. Cupiditate reprehenderit fuga praesentium voluptatibus voluptate corporis? Esse, quod.</p>
-                </div>
+        </div>
+    </div>
+</section>
+<section class="seccionGeneral">
+    <div class="contenedorGeneral">
+        <h2 class="textCenter">Testimonios</h2>
+        <div class="testimonios ">
+            <div class="testimoniosDos textoTestimonios">
+                <div class="borde" style="float: left; margin-top:20px;">
+                    <div class="imagenGaleria borde" style="background: url('img/personaUno.jpg'); background-size: cover; background-position-y: 27%; background-position-x: center; float: left;">
+                        <img src="img/cuadrado.png" class="w100">
+                    </div>
+                    <div class="">
+                        <p class="textoTestimonios">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio error sed dolorem nihil, obcaecati nesciunt! Animi, cumque? Quos dicta dolores minus distinctio adipisci cum at aperiam maiores cumque, ratione sapiente?</p>
+                    </div>
                 </div>
                 
+                <div class="borde" style="float: left; margin-top:20px;">
+                    
+                    <div class="imagenGaleria borde" style="background: url('img/personaUno.jpg'); background-size: cover; background-position-y: 27%; background-position-x: center; float: right;">
+                        <img src="img/cuadrado.png" class="w100">
+                    </div>
+                    <div class="">
+                        <p class="textoTestimonios">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio error sed dolorem nihil, obcaecati nesciunt! Animi, cumque? Quos dicta dolores minus distinctio adipisci cum at aperiam maiores cumque, ratione sapiente?</p>
+                    </div>
+                </div>
+
+                <div class="borde" style="float: left; margin-top:20px;">
+                    <div class="imagenGaleria borde" style="background: url('img/personaUno.jpg'); background-size: cover; background-position-y: 27%; background-position-x: center; float: left;">
+                        <img src="img/cuadrado.png" class="w100">
+                    </div>
+                    <div class="">
+                        <p class="textoTestimonios">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio error sed dolorem nihil, obcaecati nesciunt! Animi, cumque? Quos dicta dolores minus distinctio adipisci cum at aperiam maiores cumque, ratione sapiente?</p>
+                    </div>
+                </div>
             </div>
 
         </div>
-    </section>
 
-   
+    </div>
+</section>
 
-    <!--<section class="seccionFooter">
-        <div class="contenedorGeneral">
-            <footer class="footerCentrado">
-                <p class="textWhite parrafo">Hecho con amor por Robert Yarce Rincon</p>
-
-                <div class="custom-element custom-element-networks">
-                    <span class="element-tag">
-                        <ul class="social-networks">
-                            <span class="textWhite parrafo">Sigueme en:</span>
-                            <br>
-                            <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.facebook.com/Rob.yarce/" target="_blank"><i
-                                        class="fab fa-facebook"></i></a></li>
-                            <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.facebook.com/Rob.yarce/" target="_blank"><i
-                                        class="fab fa-twitter"></i></a></li>
-                            <li class="list-inline-item" style="font-size:25px;color:black"><a
-                                    href="https://www.instagram.com/robertyarce/" target="_blank"><i
-                                        class="fab fa-instagram"></i></a></li>
-                        </ul>
-                    </span>
-                </div>
-                <div>
-                    <p class="parrafo textWhite">Copyright © Proyecto. Todos los derechos reservados.</p>
-                </div>
-        </div>
-
-
-        </footer>
-    </section>-->
-    <?php include __DIR__ . '/partials/footer.php'; ?>
-
+<?php include __DIR__ . '/partials/footer.php'; ?>
