c32759fd7ba479821d70fcd8659f7f0dd964ea02
Luis <luissolivares14@gmail.com>
2021-09-17 15:14:39 -0500

    Luis ultimo update

--- servicios.php
+++ servicios.php
@@ -5,79 +5,91 @@
 
       <!-- contenedor -->
         <div class="container">
-          <div class="col-12 text-center p-5 p-lg-5 m-ms-1 p-ms-2">
-            <h2 class="text">Servicios</h2>
+          <div class="col-12 text-center p-5 p-lg-5 pb-lg-4 m-ms-1 p-ms-2">
+            <h2 class="text fw-bold">Servicios</h2>
             <hr class="divider" />
           </div>
 
+               
           <!-- row principal -->
             <div class="row pb-5">
 
               <div class="col-12 col-lg-12">
                 <!-- sub-row1 -->
-                <div class="row1 row cok-12 fondo rounded m-2">
+                <div class="row1 row col-12 fondo rounded m-2 bg-light shadow-lg p-3">
 
                   <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/colleague-party.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Cumpleaños</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consect  dolor epsum adipisicing elit. Voluptatum, qui!</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Cumpleaños</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consect  dolor epsum adipisicing elit. Voluptatum, qui!</p>
                       </div>
                     </div>
                   </div>
 
                   <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/aniversario.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Aniversarios</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Aniversarios</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!</p>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/Children-party.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Fiestas infantiles</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequuntur.</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Fiestas infantiles</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequuntur.</p>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/Propuesta.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Declaraciones</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, quas.</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Declaraciones</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, quas.</p>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/despedidas.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Despedidas</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, nesciunt.</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Despedidas</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, nesciunt.</p>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
-                    <div class="card col-4" style="width: 18rem;">
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="bg-dark card col-4" style="width: 18rem;">
                       <img src="./img/friends-dinner.jfif" class="card-img-top" alt="...">
                       <div class="card-body">
-                        <h3 class="card-text d-flex justify-content-center">Cena de amigos</h3>
-                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, porro.</p>
+                        <h3 class="text-light card-text d-flex justify-content-center">Cena de amigos</h3>
+                        <p class="text-light card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, porro.</p>
                       </div>
                     </div>
                   </div>
+
+                       <!-- Botones -->
+                    <div class="row p-3">
+                        <div class="col-6 d-flex p-1 justify-content-end">
+                        <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=3116291954&text=Hablamos%20para%20saber%20en%20qu%C3%A9%20estas%20interesad@" role="button" style ="background-color:  #00bb2d;"> <i class="fab fa-whatsapp"></i> Contactanos: Via Whatsapp</a>
+                        </div>
+                        <div class="col-6 d-flex p-1 justify-content-start">
+                        <a class="btn btn-dark"  href="formulario.php" role="button"><i class="far fa-envelope"></i> Contactanos: Via Mensaje </a>
+                        </div>
+                    </div>
     
                 </div>
+
             </div>
          </div>
     
