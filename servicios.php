0dc0627e3d2a5d8600406c82d56c773dbce94d6a
Luis <luissolivares14@gmail.com>
2021-09-17 12:31:47 -0500

    Servicios Update

--- servicios.php
+++ servicios.php
@@ -1,5 +1,88 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
+     <!-- Personal -->
+     <section class="page-section" id="Servicios">
+
+      <!-- contenedor -->
+        <div class="container">
+          <div class="col-12 text-center p-5 p-lg-5 m-ms-1 p-ms-2">
+            <h2 class="text">Servicios</h2>
+            <hr class="divider" />
+          </div>
+
+          <!-- row principal -->
+            <div class="row pb-5">
+
+              <div class="col-12 col-lg-12">
+                <!-- sub-row1 -->
+                <div class="row1 row cok-12 fondo rounded m-2">
+
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/colleague-party.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Cumpleaños</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consect  dolor epsum adipisicing elit. Voluptatum, qui!</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 col-md-6 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/aniversario.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Aniversarios</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus!</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/Children-party.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Fiestas infantiles</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequuntur.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/Propuesta.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Declaraciones</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, quas.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/despedidas.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Despedidas</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, nesciunt.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 col-md-12 d-flex p-3 align-items-center justify-content-center">
+                    <div class="card col-4" style="width: 18rem;">
+                      <img src="./img/friends-dinner.jfif" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h3 class="card-text d-flex justify-content-center">Cena de amigos</h3>
+                        <p class="card-text d-flex text-center fst-italic justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, porro.</p>
+                      </div>
+                    </div>
+                  </div>
+    
+                </div>
+            </div>
+         </div>
+    
+    </section>
+
 
 
 
