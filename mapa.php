470a7cae68eef9f26146df72d56c37b3f00d1b6a
Gregory <gregsoninnovo@gmail.com>
2021-09-18 16:01:43 -0500

    Add map site

--- mapa.php
+++ mapa.php
@@ -1,64 +1,29 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
 
+<section class="o-mapa-container">
+    <h1>Mapa del Sitio</h1>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <section class="o-mapa-content">
+        <ul class="o-mapa-list">
+            <li>
+                <a class="o-mapa-item" href="index.php">Inicio</a>
+            </li>
+            <li>  
+                <a class="o-mapa-item" href="nosotros.php">Nosotros</a>
+            </li>
+            <li>
+                <a class="o-mapa-item" href="carta.php">Menú</a>              
+            </li>
+            <li>
+                <a class="o-mapa-item" href="servicios.php">Servicios</a>     
+            </li>
+            <li>
+                <a class="o-mapa-item" href="formulario.php">Contáctanos</a>
+            </li>
+        </ul>
+    </section>
+</section>
 
 
 <?php include __DIR__ . '/partials/footer.php'; ?>
