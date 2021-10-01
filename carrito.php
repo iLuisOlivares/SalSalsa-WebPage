42de3e84e40f34723578e94ed28d17defb1f97b6
Gregory <gregsoninnovo@gmail.com>
2021-10-01 10:01:24 -0500

    Carrito bucle y mejora de estilos

--- carrito.php
+++ carrito.php
@@ -1,81 +1,46 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
 
 
-<h2 class="o-title-menu">Carrito de compra</h2>
+<h2 class="o-title-carrito">Carrito de compra</h2>
 
-<!-- items del carrito -->
+<?php
+    /* Carga los datos */
+    $employees = array('John', 'Michelle', 'Mari', 'Luke', 'Nellie');
+    // $data = getLocalstorage();
+    // $data = JSON.parse(localStorage.getItem("Carrito")).length;
+?>
 
 <!-- Contenedor de los items -->
 <main class="o-carrito-container">
 
     <section>
-        <!-- Item -->
-        <div class="card mb-3 o-fix-card-carrito" >
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img src="./assets/img/menu/img1.jpg" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <section class="o-item-delete-style">
-                            <h5 class="card-title">Titulo</h5>
-                            <button type="button" class="btn btn-danger">Eliminar
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
-                        </section>
-                        <p class="card-text">Combo de hamburguesa, papas y sin refresco.</p>
-                        <p class="card-text">$9.999</p>
-                        <input type="number" id="id_cantidad" class="shoppingImput" value=1 style="max-width: 3rem;border: 2px solid rgba(182, 6, 6, 0.8); border-radius: 5px; margin-left: 1px" min=1 />
+        <!-- Empieza el bucle, el cual recorre el array-->
+        <?php foreach ($employees as $employee) { ?>
+            <!-- Item -->
+            <div class="card mb-3 o-fix-card-carrito" >
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="./assets/img/menu/img1.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <section class="o-item-delete-style">
+                                <h5 class="card-title">Titulo</h5>
+                                <button type="button" class="btn btn-danger" onclick="alert('¿Estas seguro de eliminar este producto?')">Eliminar
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </section>
+                            <p class="card-text">Combo de hamburguesa, papas y sin refresco.</p>
+                            <p class="card-text">$9.999</p>
+                            <input type="number" id="id_cantidad" class="shoppingImput" value=1 style="max-width: 3rem;border: 2px solid rgba(182, 6, 6, 0.8); border-radius: 5px; margin-left: 1px" min=1 />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- Item -->
-        <div class="card mb-3 o-fix-card-carrito" >
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img src="./assets/img/menu/img1.jpg" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <section class="o-item-delete-style">
-                            <h5 class="card-title">Titulo</h5>
-                            <button type="button" class="btn btn-danger">Eliminar
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
-                        </section>
-                        <p class="card-text">Combo de hamburguesa, papas y sin refresco.</p>
-                        <p class="card-text">$9.999</p>
-                        <input type="number" id="id_cantidad" class="shoppingImput" value=1 style="max-width: 3rem;border: 2px solid rgba(182, 6, 6, 0.8); border-radius: 5px; margin-left: 1px" min=1 />
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Item -->
-        <div class="card mb-3 o-fix-card-carrito" >
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img src="./assets/img/menu/img1.jpg" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <section class="o-item-delete-style">
-                            <h5 class="card-title">Titulo</h5>
-                            <button type="button" class="btn btn-danger">Eliminar
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
-                        </section>
-                        <p class="card-text">Combo de hamburguesa, papas y sin refresco.</p>
-                        <p class="card-text">$9.999</p>
-                        <input type="number" id="id_cantidad" class="shoppingImput" value=1 style="max-width: 3rem;border: 2px solid rgba(182, 6, 6, 0.8); border-radius: 5px; margin-left: 1px" min=1 />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php }  ?>
 
 
-
-        <a href="carta.php"><button type="button">Continuar comprando</button></a>
+        <a href="carta.php"><button type="button" class="botones o-btn-bottom-carrito">Continuar comprando</button></a>
     </section>
 
     <!-- Contenedor para pagar todo -->
@@ -84,9 +49,10 @@
         <div class="card o-fix-pay-carrito">
             <div class="card-body">
                 <h5 class="card-title">Total</h5>
+                <!-- Cambiar el valor al total de todas  -->
                 <p class="card-text">$80.000</p>
                 <button id="btn__traer" type="button" class="btn btn-success o-btn-style">
-                    Agregar al carrito
+                    Pagar ahora
                 </button>
             </div>
         </div>
