4ff47d42eeaf99e9ee3ddbf676951a090be5444f
Gregory <gregsoninnovo@gmail.com>
2021-10-22 14:54:44 -0500

    modal implementation

--- src/containers/Carta/Carta.js
+++ src/containers/Carta/Carta.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import "./Carta.css";
 import ItemPlato from "../../components/ItemPlato";
+
 /* 
   Container para mostrar el diseño de la página Carta
 */
@@ -20,194 +21,214 @@ import img12 from "../../assets/static/img/img12.jpg";
 import img13 from "../../assets/static/img/img13.jpg";
 import img14 from "../../assets/static/img/img14.png";
 
-const Carta = () => (
-  <Fragment>
-    {/* Main content */}
-    <main className="main-carta">
-      {/* Título del container  */}
-      <h2 className="o-title-menu">El menú</h2>
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 
-      {/* El container del menú */}
-      <div className="container">
-        {/* Carrosel items */}
-        <div id="carouselExampleIndicators" className="carousel slide o-fix">
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="row row-cols-1 row-cols-md-3 g-4">
-                {/* Lista 1 */}
+var Carousel = require("react-responsive-carousel").Carousel;
 
-                <ItemPlato
-                  tituloPlato="Combo 1 hamburguesa"
-                  descripcionPlato="Combo de hamburguesa, con papas y refresco."
-                  imgPlato={img1}
-                  idPlato={1}
-                />
-                <ItemPlato
-                  tituloPlato="Combo 2 hamburguesa"
-                  descripcionPlato="Combo de hamburguesa, papas y sin refresco."
-                  imgPlato={img2}
-                  idPlato={2}
-                />
-                <ItemPlato
-                  tituloPlato="Hamburguesa sencilla"
-                  descripcionPlato="Hamburguesa con carne, tomate y vegetales."
-                  imgPlato={img3}
-                  idPlato={3}
-                />
-                <ItemPlato
-                  tituloPlato="Combo Hot Dog"
-                  descripcionPlato="Combo 1 de Hot dog y papas."
-                  imgPlato={img4}
-                  idPlato={4}
-                />
-                <ItemPlato
-                  tituloPlato="Hot Dog Sencillo"
-                  descripcionPlato="Hot dog sencillo con salsa."
-                  imgPlato={img5}
-                  idPlato={5}
-                />
-                <ItemPlato
-                  tituloPlato="Copa de papas sencillo"
-                  descripcionPlato="Copa de papas con guacamole."
-                  imgPlato={img6}
-                  idPlato={6}
-                />
-                <ItemPlato
-                  tituloPlato="Papas sencillas"
-                  descripcionPlato="Papas a la francesa sencilla."
-                  imgPlato={img7}
-                  idPlato={7}
-                />
-                <ItemPlato
-                  tituloPlato="2 Latas de Coca Cola"
-                  descripcionPlato="Latas de Coca Cola, con limón y hielo."
-                  imgPlato={img8}
-                  idPlato={8}
-                />
-                <ItemPlato
-                  tituloPlato="Jarra con limonada"
-                  descripcionPlato="Jarra con limonada de 1/2 litro."
-                  imgPlato={img9}
-                  idPlato={9}
-                />
-                <ItemPlato
-                  tituloPlato="Jarra de agua"
-                  descripcionPlato="Jarra de agua de 1/2 litro"
-                  imgPlato={img10}
-                  idPlato={10}
-                />
-                <ItemPlato
-                  tituloPlato="Pizza de Italiana"
-                  descripcionPlato="Pizza Italiana con 8 porciones."
-                  imgPlato={img11}
-                  idPlato={11}
-                />
-                <ItemPlato
-                  tituloPlato="Pizza personal Italiana"
-                  descripcionPlato="Pizza Italiana para 1 persona."
-                  imgPlato={img12}
-                  idPlato={12}
-                />
+class Carta extends React.Component {
+  render() {
+    const settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+    };
+    // var elements = document.getElementsByClassName("slick-active");
+
+    // for (var i = 0; i < elements.length; i++) {
+    //   elements[i].addEventListener("click", myFunction, false);
+    // }
+
+    // var myFunction = function () {
+    //   var attribute = this.getAttribute("data-myattribute");
+    //   alert("attribute");
+    // };
+
+
+
+    return (
+      <Fragment>
+        {/* Main content */}
+        <main className="main-carta">
+          {/* Título del container  */}
+          <h2 className="o-title-menu">El menú</h2>
+
+          {/* El container del menú */}
+          <div className="container">
+            {/* Carrosel items */}
+            <div>
+              <Slider {...settings}>
+                <div className="row row-cols-md-3 g-4 o-fix-column">
+                  <ItemPlato
+                    tituloPlato="Combo 1 hamburguesa"
+                    descripcionPlato="Combo de hamburguesa, con papas y refresco."
+                    imgPlato={img1}
+                    idPlato={1}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Combo 2 hamburguesa"
+                    descripcionPlato="Combo de hamburguesa, papas y sin refresco."
+                    imgPlato={img2}
+                    idPlato={2}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Hamburguesa sencilla"
+                    descripcionPlato="Hamburguesa con carne, tomate y vegetales."
+                    imgPlato={img3}
+                    idPlato={3}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Combo Hot Dog"
+                    descripcionPlato="Combo 1 de Hot dog y papas."
+                    imgPlato={img4}
+                    idPlato={4}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Hot Dog Sencillo"
+                    descripcionPlato="Hot dog sencillo con salsa."
+                    imgPlato={img5}
+                    idPlato={5}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Copa de papas sencillo"
+                    descripcionPlato="Copa de papas con guacamole."
+                    imgPlato={img6}
+                    idPlato={6}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Papas sencillas"
+                    descripcionPlato="Papas a la francesa sencilla."
+                    imgPlato={img7}
+                    idPlato={7}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="2 Latas de Coca Cola"
+                    descripcionPlato="Latas de Coca Cola, con limón y hielo."
+                    imgPlato={img8}
+                    idPlato={8}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Jarra con limonada"
+                    descripcionPlato="Jarra con limonada de 1/2 litro."
+                    imgPlato={img9}
+                    idPlato={9}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Jarra de agua"
+                    descripcionPlato="Jarra de agua de 1/2 litro"
+                    imgPlato={img10}
+                    idPlato={10}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Pizza de Italiana"
+                    descripcionPlato="Pizza Italiana con 8 porciones."
+                    imgPlato={img11}
+                    idPlato={11}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Pizza personal Italiana"
+                    descripcionPlato="Pizza Italiana para 1 persona."
+                    imgPlato={img12}
+                    idPlato={12}
+                    
+                  />
+                </div>
 
                 {/* close */}
-              </div>
+                <div className="row row-cols-md-3 g-4 o-fix-column-2">
+                  <ItemPlato
+                    tituloPlato="Copa de helado"
+                    descripcionPlato="Copa de helado de 1 sabor con uvas."
+                    imgPlato={img13}
+                    idPlato={13}
+                    
+                  />
+                  <ItemPlato
+                    tituloPlato="Cono de helado"
+                    descripcionPlato="ono de helado con 2 bolas de diferentes sabores."
+                    imgPlato={img14}
+                    idPlato={14}
+                    
+                  />
+                </div>
+              </Slider>
             </div>
-            <div className="carousel-item">
-              <div className="row row-cols-1 row-cols-md-3 g-4">
-                <ItemPlato
-                  tituloPlato="Copa de helado"
-                  descripcionPlato="Copa de helado de 1 sabor con uvas."
-                  imgPlato={img13}
-                  idPlato={13}
+          </div>
+        </main>
+
+        {/* Link to download the menu  */}
+        <div className="o-menu-doc">
+          <a href="./assets/doc/Menu_Completo.pdf" download="menu">
+            Descarga el Menú completo aquí
+          </a>
+        </div>
+
+        {/* The Modal  */}
+        <div id="myModal" className="modal">
+          {/* Modal content */}
+          <div className="modal-content">
+            <span className="close">&times;</span>
+            <h1 id="o-value-plato-titulo">Título del plato</h1>
+            <span id="o-value-plato">num</span>
+            <p id="o-value-plato-descripcion">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              aspernatur excepturi error temporibus odit, quod alias fuga.
+              Corporis ipsa veniam qui ipsam rem atque voluptate nihil vero?
+              Libero, illo totam.
+            </p>
+            <div className="d-flex justify-content-center">
+              <div
+                id="o-value-plato-image"
+                className="img-thumbnail rounded o-img-modal-container"
+                style={{ width: 720 }}
+              ></div>
+            </div>
+            <div className="o-information-cost">
+              {/* Precio */}
+              <p>Precio</p>
+              <span id="o-value-plato-price">Error</span>
+              <section className="o-style-btn-add-carrito">
+                {/* Open whatsapp to send a message */}
+                <button
+                  type="button"
+                  className="botones"
+                  onclick={
+                    "location.href='https://api.whatsapp.com/send?phone=3116291954&text=Hablamos%20para%20saber%20en%20qu%C3%A9%20estas%20interesad@'"
+                  }
+                >
+                  Ver contacto
+                </button>
+                <button id="btn__agregar" type="button" className="botones">
+                  Agregar al carrito <i className="fas fa-shopping-cart" />
+                </button>
+                <input
+                  type="number"
+                  id="id_cantidad"
+                  className="shoppingImput"
+                  value={1}
+                  className="o-input-carta"
+                  min={1}
                 />
-                <ItemPlato
-                  tituloPlato="Cono de helado"
-                  descripcionPlato="ono de helado con 2 bolas de diferentes sabores."
-                  imgPlato={img14}
-                  idPlato={14}
-                />
-                {/* close */}
-              </div>
+              </section>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-
-    {/* Link to download the menu  */}
-    <div className="o-menu-doc">
-      <a href="./assets/doc/Menu_Completo.pdf" download="menu">
-        Descarga el Menú completo aquí
-      </a>
-    </div>
-
-    {/* The Modal  */}
-    <div id="myModal" className="modal">
-      {/* Modal content */}
-      <div className="modal-content">
-        <span className="close">&times;</span>
-        <h1 id="o-value-plato-titulo">Título del plato</h1>
-        <span id="o-value-plato">num</span>
-        <p id="o-value-plato-descripcion">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-          aspernatur excepturi error temporibus odit, quod alias fuga. Corporis
-          ipsa veniam qui ipsam rem atque voluptate nihil vero? Libero, illo
-          totam.
-        </p>
-        <div className="d-flex justify-content-center">
-          <div
-            id="o-value-plato-image"
-            className="img-thumbnail rounded o-img-modal-container"
-            style={{ width: 720 }}
-          ></div>
-        </div>
-        <div className="o-information-cost">
-          {/* Precio */}
-          <p>Precio</p>
-          <span id="o-value-plato-price">Error</span>
-          <section className="o-style-btn-add-carrito">
-            {/* Open whatsapp to send a message */}
-            <button
-              type="button"
-              className="botones"
-              onclick={
-                "location.href='https://api.whatsapp.com/send?phone=3116291954&text=Hablamos%20para%20saber%20en%20qu%C3%A9%20estas%20interesad@'"
-              }
-            >
-              Ver contacto
-            </button>
-            <button id="btn__agregar" type="button" className="botones">
-              Agregar al carrito <i className="fas fa-shopping-cart" />
-            </button>
-            <input
-              type="number"
-              id="id_cantidad"
-              className="shoppingImput"
-              value={1}
-              className="o-input-carta"
-              min={1}
-            />
-          </section>
-        </div>
-      </div>
-    </div>
-  </Fragment>
-);
+      </Fragment>
+    );
+  }
+}
 
 export default Carta;
