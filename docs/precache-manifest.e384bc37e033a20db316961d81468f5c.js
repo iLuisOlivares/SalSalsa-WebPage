bd97252cd126bf21b57c9f427e71f130bfee66a8
Luis Olivares <luissolivaresp@gmail.com>
2022-01-20 19:45:00 -0500

    rename build folder

---
+++ docs/precache-manifest.e384bc37e033a20db316961d81468f5c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066a998ababb160bf2133bcd4075ad21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba4957c33b958cbee9c",
+    "url": "/static/css/2.85bb1a1b.chunk.css"
+  },
+  {
+    "revision": "61719cd9988ac1fe6b05",
+    "url": "/static/css/main.dab48e93.chunk.css"
+  },
+  {
+    "revision": "aba4957c33b958cbee9c",
+    "url": "/static/js/2.8d1ccc30.chunk.js"
+  },
+  {
+    "revision": "0e3a3f3a32170eaa364f30d7c8df2ba8",
+    "url": "/static/js/2.8d1ccc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61719cd9988ac1fe6b05",
+    "url": "/static/js/main.51aacb51.chunk.js"
+  },
+  {
+    "revision": "ae53e319345477e101aa",
+    "url": "/static/js/runtime-main.865bd490.js"
+  },
+  {
+    "revision": "edb7f34a467f93706b5cef1542f749bb",
+    "url": "/static/media/LogoTipo.edb7f34a.png"
+  },
+  {
+    "revision": "10ed3436bbd00ad19781a60489078e76",
+    "url": "/static/media/fondo.10ed3436.jfif"
+  },
+  {
+    "revision": "c3daaf80cba6bdccd9a01eb5e25c1064",
+    "url": "/static/media/img1.c3daaf80.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
